52cfb67254c4d58c3c3c87377605b0c68e402058
Christiano0407 <placebo0704@gmail.com>
2022-07-12 11:34:06 -0500

    http

--- sincronismo/js/app.js
+++ sincronismo/js/app.js
@@ -1,1 +1,42 @@
 console.log("Hello");
+
+//** == Callbacks == */
+//** === */
+const saludar = (nombre) => {
+  console.log(`Hola,  ${nombre}`);
+};
+
+const callbackSaludar = (callback) => {
+  let nombre = "soy Chris Velázquez";
+  callback(nombre);
+};
+
+//callbackSaludar(saludar);
+//** === */
+const amigoForever = (amistad) => {
+  console.log(`Los años de amistad que tengo con Mau, son: ${amistad}`);
+};
+
+const amigaForever = (callbacks) => {
+  let amistad = `Veinte años de amistad pura`;
+  callbacks(amistad);
+};
+
+//amigaForever(amigoForever);
+//** === */
+const sumOne = (num1, num2) => {
+  return num1 + num2;
+};
+
+const sumTotal = (num1, num2, callback) => {
+  return callback(num1, num2);
+};
+
+//console.log(sumTotal(25, 5, sumOne));
+
+//** === */
+function helloGirl(name) {
+  console.log(`Hello, ${name}`);
+}
+
+setTimeout(helloGirl, 2000, ` Alma`);
---
+++ sincronismo/js/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+function helloGirl(name) {
+  console.log(`Hello, ${name}`);
+}
+
+setTimeout(helloGirl, 2000, ` Alma`);
