bd14df2288397b3671f858097070584896f3416d
Chris_Velázquez04 <placebo0704@gmail.com>
2022-07-18 21:15:09 +0000

    add async & await

--- sincronismo/js/asyncWait.js
+++ sincronismo/js/asyncWait.js
@@ -1,3 +1,22 @@
+//** === Async & Await === */
+const fnAsync = () => {
+  return new Promise((resolve, reject) => {
+    true
+      ? setTimeout(() => resolve("Async New"), 2000)
+      : reject(new Error("Error!"));
+  });
+};
+
+const fnAsyncAwait = async () => {
+  const play = await fnAsync();
+  console.log(play);
+  console.log("Hello!");
+};
+console.log("Before");
+fnAsyncAwait();
+console.log("After");
+
+//** ===Async Functions == Try and Catch === */
 import fetch from "node-fetch";
 const API = `https://api.escuelajs.co/api/v1`;
 
