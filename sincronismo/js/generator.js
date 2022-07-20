996e31612532ffed95597f1a8cb789b932b2a7f9
Chris_Velázquez04 <placebo0704@gmail.com>
2022-07-20 14:26:29 +0000

    generators practices

--- sincronismo/js/generator.js
+++ sincronismo/js/generator.js
@@ -21,3 +21,34 @@ console.log(it.next().value);
 console.log(it.next().value);
 console.log(it.next().value);
 console.log(it.next().value);
+
+//** === Generator API === */
+import fetch from "node-fetch";
+const API = `https://api.escuelajs.co/api/v1`;
+
+const dataAPI = async (urlAPI) => {
+  const response = await fetch(urlAPI);
+  const data = await response.json();
+  return data;
+  //console.log(data);
+};
+
+async function* consumerAPI(urlAPI) {
+  try {
+    const products = await dataAPI(`${urlAPI}/products`);
+    yield console.log(products);
+    const product = await dataAPI(`${urlAPI}/products/${products[0].id}`);
+    yield console.log(product.title);
+    yield console.log(product.price);
+    const category = await dataAPI(`${urlAPI}/categories/${product.category.id} `); 
+    yield console.log(category.name); 
+  } catch (error) {
+    console.log("We have an error!");
+  }
+}
+
+const dg = consumerAPI(API);
+console.log(dg.next().value);
+console.log(dg.next().value);
+console.log(dg.next().value);
+console.log(dg.next().value);
--- sincronismo/js/tempCodeRunnerFile.js
+++ sincronismo/js/tempCodeRunnerFile.js
@@ -1,10 +1,29 @@
-function* iterate(array) {
-  for (let value of array) {
-    yield value;
+import fetch from "node-fetch";
+const API = `https://api.escuelajs.co/api/v1`;
+
+const dataAPI = async (urlAPI) => {
+  const response = await fetch(urlAPI);
+  const data = await response.json();
+  return data;
+  //console.log(data);
+};
+
+async function* consumerAPI(urlAPI) {
+  try {
+    const products = await dataAPI(`${urlAPI}/products`);
+    yield console.log(products);
+    const product = await dataAPI(`${urlAPI}/products/${products[0].id}`);
+    yield console.log(product.title);
+    yield console.log(product.price);
+    const category = await dataAPI(`${urlAPI}/categories/${product.category.id} `); 
+    yield console.log(category.name); 
+  } catch (error) {
+    console.log("We have an error!");
   }
 }
-const it = iterate([`Pamela`, `Luisa`, `Natalia`, `Mariana`]);
-console.log(it.next().value);
-console.log(it.next().value);
-console.log(it.next().value);
-console.log(it.next().value);
+
+const dg = consumerAPI(API);
+console.log(dg.next().value);
+console.log(dg.next().value);
+console.log(dg.next().value);
+console.log(dg.next().value);
