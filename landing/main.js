ed7a68564af32e9cad8b48a8eef31ffb504c86e3
Chris_Velázquez04 <placebo0704@gmail.com>
2022-07-22 17:25:26 +0000

    travel API

--- landing/main.js
+++ landing/main.js
@@ -1,1 +1,14 @@
 console.log("Landing Page");
+
+//** == const == */
+const contentAPI = document.querySelector(`.group relative`);
+const API = ` `; 
+
+// ** == Call API ==*/
+const apiData = async  (urlAPI) => {
+ const response = await  fetch(urlAPI); 
+ const data = await response.json(); 
+ return data; 
+}
+
+
